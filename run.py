cea50505af43c65be8c21f284099b28f07725483
w16311 <w16311@gmail.com>
2023-03-30 18:33:24 +0800

    add cycle

--- run.py
+++ run.py
@@ -1,29 +1,39 @@
+import os
+import glob
 from train import *
 from Utils import *
 import nibabel as nib
 
 def main():
-    fixed = load_nii('/home/user/Documents/dataset/Mindboggle101/mindboggle/image_in_MNI152_normalized/OASIS-TRT-20-1.nii.gz')
-    moving = load_nii('/home/user/Documents/dataset/Mindboggle101/mindboggle/image_in_MNI152_normalized/OASIS-TRT-20-12.nii.gz')
-    # fixed = load_nii('/home/user/Documents/dataset/oasis_seg/OASIS_OAS1_0005_MR1/aligned_norm.nii.gz')
-    # moving = load_nii('/home/user/Documents/dataset/oasis_seg/OASIS_OAS1_0011_MR1/aligned_norm.nii.gz')
-    # fixed = fixed[9:173, 13:205, 19:163]
-    # moving = moving[9:173, 13:205, 19:163]
-    fixed_seg = load_nii('/home/user/Documents/dataset/Mindboggle101/mindboggle/label_31_reID_merged/OASIS-TRT-20-1.nii.gz')
-    moving_seg = load_nii('/home/user/Documents/dataset/Mindboggle101/mindboggle/label_31_reID_merged/OASIS-TRT-20-12.nii.gz')
-    # fixed_seg = load_nii('/home/user/Documents/dataset/oasis_seg/OASIS_OAS1_0005_MR1/aligned_seg35.nii.gz')
-    # moving_seg = load_nii('/home/user/Documents/dataset/oasis_seg/OASIS_OAS1_0011_MR1/aligned_seg35.nii.gz')
-    # fixed_seg = fixed_seg[9:173, 13:205, 19:163]
-    # moving_seg = moving_seg[9:173, 13:205, 19:163]
-    
-    im_warped, seg_warped, dice = train([fixed, moving], [fixed_seg, moving_seg], T = 7, n_epoch=300)
-    tmp = nib.load('/home/user/Documents/dataset/Mindboggle101/mindboggle/label_31_reID_merged/OASIS-TRT-20-1.nii.gz')
-    seg_affine = tmp.affine
-    nib.Nifti1Image(im_warped,seg_affine).to_filename('im_warped.nii.gz')
-    nib.Nifti1Image(seg_warped,seg_affine).to_filename('seg_warped.nii.gz')
+    path_img = "/home/user/Documents/dataset/Mindboggle101/mindboggle/image_in_MNI152_normalized"
+    path_seg = "/home/user/Documents/dataset/Mindboggle101/mindboggle/label_31_reID_merged"
+    img_file = []
+    seg_file = [] 
+    dice_all = []
+    files = glob.glob(os.path.join(path_seg,"*.nii.gz"))
+    for file in files:
+        if "flipped" not in file:
+            seg_file.append(file)
+            tmp = os.path.split(file)
+            tmp = os.path.join(path_img,tmp[1])
+            img_file.append(tmp)
+    # label = [2, 3, 4, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 28, 41, 42, 43, 46, 47, 49, 50, 51, 52, 53, 54, 60]
+    label = list(range(32))
+    fixed = load_nii(img_file[0])
+    fixed_seg = load_nii(seg_file[0])
+    for i in range(1, 61):
+        moving = load_nii(img_file[i])
+        moving_seg = load_nii(seg_file[i])
+        im_warped, seg_warped, dice = train([fixed, moving], [fixed_seg, moving_seg], label, T = 7, n_epoch=300)
+        dice_all.append(dice)
+
+    # save result
+    # tmp = nib.load('/home/user/Documents/NODEO-DIR/data/OAS1_0001_MR1/brain_aseg.nii.gz')
+    # seg_affine = tmp.affine
+    # nib.Nifti1Image(im_warped,seg_affine).to_filename('im_warped.nii.gz')
+    # nib.Nifti1Image(seg_warped,seg_affine).to_filename('seg_warped.nii.gz')
 
 
-    print(dice)
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -20,14 +20,14 @@ def setup_seed(seed):
     #  random.seed(seed)
      torch.backends.cudnn.deterministic = True
 
-def train(img, seg, T = 7, n_epoch=300):
+def train(img, seg, label, T = 7, n_epoch=300):
     setup_seed(1357)
     device = torch.device('cuda:0')
     mlp = Siren(in_features=3, out_features=3, hidden_features=512, 
                       hidden_layers=3, outermost_linear=True)
     mlp.to(device)
     optimizer = torch.optim.Adam(lr=1e-4, params=mlp.parameters(), amsgrad=True)
-    loss_NCC = NCC(win=21)
+    loss_NCC = NCC(win=15)
 
     # load img
     fixed = img[0]
@@ -53,7 +53,7 @@ def train(img, seg, T = 7, n_epoch=300):
     mgrid_shape.append(3)
     for i in im_shape:
         mgrid_shape.append(int(i/2))
-    label = list(range(32))
+    # label = list(range(32))
 
     # original dice loss
     dice_move2fix = dice(seg_m.unsqueeze(0).detach().cpu().numpy(), seg_f, label)
@@ -73,10 +73,10 @@ def train(img, seg, T = 7, n_epoch=300):
         df_up =  F.grid_sample(df.unsqueeze(0), grid_f, mode='bilinear',align_corners=True) #Up sample, interpolate
 
         # Diffeomorphic Deformation as Integration (4.2 section of paper)
-        df_cur = grid_f + df_up.permute(0, 2, 3, 4, 1) / torch.pow(torch.tensor(2), T) 
+        df_cur = grid_f + 0.1 * df_up.permute(0, 2, 3, 4, 1) / torch.pow(torch.tensor(2), T) 
         df_cur = F.grid_sample(df.unsqueeze(0), df_cur, mode='bilinear',align_corners=True)        
         for j in range(T, 0, -1):
-             df_next = grid_f + df_cur.permute(0, 2, 3, 4, 1) / torch.pow(torch.tensor(2), j)
+             df_next = grid_f + 0.1 * df_cur.permute(0, 2, 3, 4, 1) / torch.pow(torch.tensor(2), j)
              df_cur = F.grid_sample(df.unsqueeze(0), df_next, mode='bilinear',align_corners=True)
         grid_warped = grid_f + 0.1 * df_cur.permute(0, 2, 3, 4, 1)
 
@@ -92,7 +92,7 @@ def train(img, seg, T = 7, n_epoch=300):
         # loss_sim = loss_mse(im_warped, im_f)
         # neg Jacobian loss
         # loss_J = 100 * neg_Jdet_loss(df.unsqueeze(0).permute(0, 2, 3, 4, 1))
-        loss_J = 100 * neg_Jdet_loss(phi.unsqueeze(0).permute(0, 2, 3, 4, 1))
+        loss_J = 10 * neg_Jdet_loss(phi.unsqueeze(0).permute(0, 2, 3, 4, 1))
         # phi dphi/dx loss
         loss_df = 0.1 * smoothloss_loss(df.unsqueeze(0))
         loss = loss_sim + loss_J + loss_df
